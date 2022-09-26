319492f300354f6330ebd91a0cfcfadec0c9d64f
JCCiasico <carlociasico01@gmail.com>
2022-09-26 11:14:05 +0800

    chore: destructured the props

--- src/components/cats.jsx
+++ src/components/cats.jsx
@@ -1,19 +1,19 @@
 import './cats.css'
-import phone from '../assets/phone-icon.png'
-import mail from '../assets/mail-icon.png'
+import phoneIcon from '../assets/phone-icon.png'
+import mailIcon from '../assets/mail-icon.png'
 
-export default function Cat(props) {
+export default function Cat({image,name,phone,email}) {
     return (
         <div className="contact-card">
-            <img src={props.image} />
-            <h3>{props.name}</h3>
+            <img src={image} />
+            <h3>{name}</h3>
             <div className="info-group">
-                <img src={phone} />
-                <p>{props.phone}</p>
+                <img src={phoneIcon} />
+                <p>{phone}</p>
             </div>
             <div className="info-group">
-                <img src={mail} />
-                <p>{props.email}</p>
+                <img src={mailIcon} />
+                <p>{email}</p>
             </div>
         </div>
     );
